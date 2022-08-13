8a9eab0c902d5d86c63e4d2f64f3a46d50c3def8
LoginmyWorld <98610167+Nagesh1001@users.noreply.github.com>
2022-08-13 23:03:56 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, {Component, useState} from "react";
+import "./../styles/App.css";
+
+function App() {
+  const [show,setShow] = useState(false);
+  return (
+    <div id="main">
+      <button id="click" onClick={()=>setShow(true)}>Click me</button>
+      {show && (
+        <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+        </p>
+      ) }
+    </div>
+  );
+}
+
+
+export default App;
